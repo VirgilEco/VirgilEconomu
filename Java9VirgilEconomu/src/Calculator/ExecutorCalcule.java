1af711439092e680ba455064a4f5c061fc71c777
Virgil <Virgil@DESKTOP-PN64REC>
2018-09-13 17:08:18 +0300

    Merge origin/master

---
+++ Java9VirgilEconomu/src/Calculator/ExecutorCalcule.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Virgil
+ */
+public class ExecutorCalcule {
+
+ 
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Calculator unCalculator = new Calculator ();
+       
+       System.out.println("Introduceti primul numar:");
+       Scanner sc = new Scanner(System.in);
+       int i = sc.nextInt();
+       
+       System.out.println("Introduceti al doilea numar:");
+       Scanner sc1 = new Scanner(System.in);
+       int j = sc1.nextInt();
+       
+       int iesireAdunare = unCalculator.aduna(i, j);
+       System.out.println(iesireAdunare);
+             
+       int iesireScadere = unCalculator.scade(i,j);
+       System.out.println(iesireScadere);
+       
+       int iesireImpartire = unCalculator.imparte(i,j);
+       System.out.println(iesireImpartire);
+       
+       int iesireInmultire = unCalculator.inmulteste(i,j);
+       System.out.println(iesireInmultire);
+    }
+
+}
+
+
